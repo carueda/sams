69b71db98828df51ae3bec699abeefc54549c806
Carlos Rueda <carueda@gmail.com>
2003-08-14 07:27:23 +0000

    no longer extends ptolemy.plot.Plot but delegates on it

--- samsgui/src/samsgui/dbgui/Plot.java
+++ samsgui/src/samsgui/dbgui/Plot.java
@@ -15,113 +15,287 @@ import java.awt.geom.AffineTransform;
 import javax.swing.*;
 import java.util.*;
 import java.util.List;
+import java.awt.print.Printable;
 
 /**
- * A Plot.
- * This extends ptolemy.plot.Plot with convenience functionality.
+ * A panel with plotting capabilities.
+ * Current implementation delegates main operations on a ptolemy.plot.Plot
+ * object (which is nice but has some limitations).
  * @author Carlos A. Rueda
  * @version $Id$ 
  */
-public class Plot extends ptolemy.plot.Plot {
+public class Plot extends JPanel {
+	private SubPtolemyPlot ptplot;
 	private DbGui guidb;
 	private static RenderingHints hints = null;
 	
 	/** Selected spectrum elements. */
-	Hashtable selected;
+	private Hashtable selected;
 
-	int currentDataset;
+	private int currentDataset;
 
-	MyButtonListener buttonListener;
+	private MyButtonListener buttonListener;
 	
-	LegendPanel legendPanel;
-	JDialog legendsFrame;
+	private JDialog legendsFrame;
 	
-	boolean antiAliased;
+	private boolean antiAliased;
 	
 	/**  Constructor */
-	public Plot(DbGui guidb)
-	{
-		super();
+	public Plot(DbGui guidb) {
+		super(new GridLayout(1,1));
+		add(ptplot = new SubPtolemyPlot());
 		this.guidb = guidb;
 		currentDataset = 0;
 		selected = new Hashtable();
-		setXLabel("Wavelength");
-		setYLabel("Reflectance");
-		addMouseMotionListener(new MyMouseMotionListener());
+		ptplot.setXLabel("Wavelength");
+		ptplot.setYLabel("Reflectance");
 		//setButtons(true);
 		buttonListener = new MyButtonListener();
 		antiAliased = false;
 	}
 
-	/** Now, to show the current location in term of actual units. */
-    public class MyMouseMotionListener implements MouseMotionListener  {
-		private void move(MouseEvent ev) {
-			int x = ev.getX();
-			int y = ev.getY();
-			if ( x < _ulx || x > _lrx  ||   y < _uly || y > _lry )
-				return;
-			
-			// translate:
-			x -= _ulx;
-			y -= _uly;
-			double xx = (double) x / _xscale + _xMin;
-			double yy = (double) _yMax - y / _yscale;
-			
-			xx = (double) Math.round(xx * 1000) / 1000;
-			yy = (double) Math.round(yy * 1000) / 1000;
-			
-			guidb.updateLocation(xx, yy);
-        }
-		
-        public void mouseDragged(MouseEvent ev) {
-			move(ev);
-		}
-		
-        public void mouseMoved(MouseEvent ev) {
-			move(ev);
-		}
-    }
-	
 	private void updateLegends() {
-		if ( legendPanel != null )
+		ptplot.updateLegends();
+	}
+	
+	public void print() throws Exception {
+		ptplot.print();
+	}
+	
+	private class SubPtolemyPlot extends ptolemy.plot.Plot { 
+		LegendPanel legendPanel;
+		
+		SubPtolemyPlot() {
+			super();
+			addMouseMotionListener(new MyMouseMotionListener());
+		}
+		
+		public void paintComponent(Graphics graphics) {
+			if ( antiAliased && graphics instanceof Graphics2D )
+				((Graphics2D) graphics).addRenderingHints(hints);
+			super.paintComponent(graphics);
+		}
+	
+		public void repaint() {
+			updateLegends();
+			super.repaint();
+		}
+
+		synchronized void zoomXRange(double minx, double maxx) {
+			double miny = Double.POSITIVE_INFINITY;
+			double maxy = Double.NEGATIVE_INFINITY;
+			for (int dataset = 0; dataset < _points.size(); dataset++) {
+				Vector points = (Vector)_points.elementAt(dataset);
+				for (int index = 0; index < points.size(); index++) {
+					PlotPoint pt = (PlotPoint)points.elementAt(index);
+					if ( minx <= pt.x && pt.x <= maxx ) {
+						if (miny > pt.y)
+							miny = pt.y;
+						if (maxy < pt.y)
+							maxy = pt.y;
+					}
+				}
+			}
+			setXRange(minx, maxx);
+			setYRange(miny, maxy);
+			ptplot.repaint();
+		}
+		
+		synchronized void zoomYRange(double miny, double maxy) {
+			double minx = Double.POSITIVE_INFINITY;
+			double maxx = Double.NEGATIVE_INFINITY;
+			for (int dataset = 0; dataset < _points.size(); dataset++) {
+				Vector points = (Vector)_points.elementAt(dataset);
+				for (int index = 0; index < points.size(); index++) {
+					PlotPoint pt = (PlotPoint)points.elementAt(index);
+					if ( minx > pt.x )
+						minx = pt.x;
+					if ( maxx < pt.x )
+						maxx = pt.x;
+				}
+			}
+			setXRange(minx, maxx);
+			setYRange(miny, maxy);
+			ptplot.repaint();
+		}
+		
+		private void reset(boolean format) {
+			currentDataset = 0;
+			selected.clear();
+			clear(format);
+			setXLabel("Wavelength");
+			setYLabel("Reflectance");
+			clearLegends();
+			updateLegends();
+		}
+	
+		/** The print method copied from Ptolemy. (class PlotBox.ButtonListener) */
+		public void printPtolemyVersion() throws Exception {
+			PrinterJob job = PrinterJob.getPrinterJob();
+			job.setPrintable(this);
+			if (job.printDialog()) 
+				job.print();
+		}
+			
+		/**
+		 * Alternative print option.
+		 * This is trying to overcome a problem with Java printing that
+		 * doesn't take the orientation specified by the user by means
+		 * of job.printDialog().  
+		 * See PlotBox.ButtonListener, DbGui.createPlotPanel.
+		 */
+		void print() throws Exception {
+			PrinterJob job = PrinterJob.getPrinterJob();
+			PageFormat defaultpage = job.defaultPage();
+			PageFormat format = job.pageDialog(defaultpage);
+			if ( format != defaultpage ) {
+				job.setPrintable(this, format);
+				job.print();
+			}
+		}
+		
+		public synchronized int print(Graphics graphics, PageFormat format, int index) 
+		throws PrinterException  {
+			if (graphics == null)
+				return Printable.NO_SUCH_PAGE;
+				
+			if (index >= 1) {      // We only print on one page.
+				return Printable.NO_SUCH_PAGE;
+			}
+			
+			Graphics2D g2d = (Graphics2D) graphics;
+			if ( antiAliased )
+				g2d.addRenderingHints(hints);
+			
+			g2d.translate((int)format.getImageableX(), (int)format.getImageableY());
+			Rectangle rect = new Rectangle(0,0, 800, 600);
+			double xscale = format.getImageableWidth() / rect.width;
+			//double yscale = format.getImageableHeight() / rect.height;
+			double yscale = xscale;
+			g2d.scale(xscale, yscale);
+			_drawPlot(graphics, true, rect);
+			return Printable.PAGE_EXISTS;
+		}
+	
+		void showLegendsWindow() {
+			if ( legendsFrame == null ) {
+				legendsFrame = new JDialog(guidb.parentFrame, false);
+				legendsFrame.setTitle("Legends");
+				legendPanel = new LegendPanel();
+				JScrollPane scroller = new JScrollPane(legendPanel);
+				scroller.setPreferredSize(new Dimension(220,200));
+				legendsFrame.getContentPane().add(scroller);
+				legendsFrame.addWindowListener(new WindowAdapter() {
+					public void windowClosing(WindowEvent e) {
+						legendsFrame.setVisible(false);
+					}
+				});
+				legendsFrame.pack();
+				legendsFrame.setLocation(200, 100);
+			}
+			else if ( legendsFrame.isShowing() ) {
+				legendsFrame.setVisible(false);
+				return;
+			}
+			legendsFrame.setVisible(true);
 			legendPanel.updateLegends();
+		}
+	
+		void updateLegends() {
+			if ( legendPanel != null )
+				legendPanel.updateLegends();
+		}
+		
+		/** A panel to show the current plot legends. */
+		class LegendPanel extends JPanel {
+			/** Updates the panel contents an calls this.revalidate() and this.repaint(). */
+			void updateLegends() {
+				// subjective values:
+				//	200: enough width for legend texts
+				//	 20: enough height for each legend text
+				
+				int nds = getNumDataSets();
+				Dimension size = new Dimension(200, 10 + 20*nds + 10);
+				setPreferredSize(size);
+				this.revalidate();
+				this.repaint();
+			}
+			
+			public void paint(Graphics graphics) {
+				if ( antiAliased && graphics instanceof Graphics2D )
+					((Graphics2D) graphics).addRenderingHints(hints);
+				super.paint(graphics);
+				Color curr_color = graphics.getColor();
+				int nds = getNumDataSets();
+				for ( int dataset = 0; dataset < nds; dataset++ ) {
+					if (_usecolor) {
+						int color = dataset % _colors.length;
+						graphics.setColor(_colors[color]);
+					}
+					int xpos = 10;
+					int ypos = 10 + dataset * 20;
+					boolean clip = false;
+					_drawPoint(graphics, dataset, xpos, ypos, clip);
+					graphics.setColor(curr_color);
+					graphics.drawString(getLegend(dataset), xpos + 10, ypos + 3);
+				}
+			}
+		}
+		
+		/** Now, to show the current location in term of actual units. */
+		class MyMouseMotionListener implements MouseMotionListener  {
+			private void move(MouseEvent ev) {
+				int x = ev.getX();
+				int y = ev.getY();
+				if ( x < _ulx || x > _lrx  ||   y < _uly || y > _lry )
+					return;
+				
+				// translate:
+				x -= _ulx;
+				y -= _uly;
+				double xx = (double) x / _xscale + _xMin;
+				double yy = (double) _yMax - y / _yscale;
+				
+				xx = (double) Math.round(xx * 1000) / 1000;
+				yy = (double) Math.round(yy * 1000) / 1000;
+				
+				guidb.updateLocation(xx, yy);
+			}
+			
+			public void mouseDragged(MouseEvent ev) {
+				move(ev);
+			}
+			
+			public void mouseMoved(MouseEvent ev) {
+				move(ev);
+			}
+		}
+		
 	}
 	
-    public void paintComponent(Graphics graphics) {
-		if ( antiAliased && graphics instanceof Graphics2D )
-			((Graphics2D) graphics).addRenderingHints(hints);
-		super.paintComponent(graphics);
-	}
-	
-	public void repaint() {
-		updateLegends();
-		super.repaint();
+	public void showLegendsWindow() {
+		ptplot.showLegendsWindow();
 	}
 	
 	/** Clears and resets all parameters to their original values. */
 	public void reset() {
-		reset(true);
+		ptplot.reset(true);
 		updateLegends();
+	}
+	
+	public void repaint() {
+		if ( ptplot != null )
+			ptplot.repaint();
 	}
 	
 	/** Clears the plot. */
 	public void clearSignatures() {
-		reset(false);
+		ptplot.reset(false);
 	}
 	
-	private void reset(boolean format) {
-		currentDataset = 0;
-		selected.clear();
-		clear(format);
-		setXLabel("Wavelength");
-		setYLabel("Reflectance");
-		clearLegends();
-		updateLegends();
-	}
-
 	/** Draws a signature making it the only one in the plot. */
 	public void setSignature(Signature sig, String legend) {
-		reset(false);
+		ptplot.reset(false);
 		addSignature(sig, legend);
 		updateLegends();
 	}
@@ -138,13 +312,13 @@ public class Plot extends ptolemy.plot.Plot {
 		if ( sig == null )
 			return;
 
-		addLegend(currentDataset, legend);
+		ptplot.addLegend(currentDataset, legend);
 		selected.put(legend, new Integer(currentDataset));
 		int size = sig.getSize();
 		Signature.Datapoint point;
 		for ( int i = 0; i < size; i++ ) {
 			point = sig.getDatapoint(i);
-			addPoint(currentDataset, point.x, point.y, true);
+			ptplot.addPoint(currentDataset, point.x, point.y, true);
 		}
 		currentDataset++;
 		
@@ -168,7 +342,7 @@ public class Plot extends ptolemy.plot.Plot {
 		if ( i == null )
 			return;
 
-		clear(i.intValue());
+		ptplot.clear(i.intValue());
 
 		// PENDING: Synchronize legends
 		// this method doesn't exist!  ;-(
@@ -179,105 +353,27 @@ public class Plot extends ptolemy.plot.Plot {
 	}
 
 	/** Zooms the current X range. This calls repaint(); */
-	public synchronized void zoomXRange() {
-		double[] r = getXRange();
+	public void zoomXRange() {
+		double[] r = ptplot.getXRange();
 		zoomXRange(r[0], r[1]);
 	}
 
 	/** Zooms the specified X range. This calls repaint(); */
-	public synchronized void zoomXRange(double minx, double maxx) {
-		double miny = Double.POSITIVE_INFINITY;
-		double maxy = Double.NEGATIVE_INFINITY;
-		for (int dataset = 0; dataset < _points.size(); dataset++) {
-			Vector points = (Vector)_points.elementAt(dataset);
-			for (int index = 0; index < points.size(); index++) {
-				PlotPoint pt = (PlotPoint)points.elementAt(index);
-				if ( minx <= pt.x && pt.x <= maxx ) {
-					if (miny > pt.y)
-						miny = pt.y;
-					if (maxy < pt.y)
-						maxy = pt.y;
-				}
-			}
-		}
-		setXRange(minx, maxx);
-		setYRange(miny, maxy);
-		repaint();
+	public void zoomXRange(double minx, double maxx) {
+		ptplot.zoomXRange(minx, maxx);
 	}
 	
 	/** Zooms the current Y range. This calls repaint(); */
-	public synchronized void zoomYRange() {
-		double[] r = getYRange();
+	public void zoomYRange() {
+		double[] r = ptplot.getYRange();
 		zoomYRange(r[0], r[1]);
 	}
 
 	/** Zooms the specified Y range. This calls repaint(); */
-	public synchronized void zoomYRange(double miny, double maxy) {
-		double minx = Double.POSITIVE_INFINITY;
-		double maxx = Double.NEGATIVE_INFINITY;
-		for (int dataset = 0; dataset < _points.size(); dataset++) {
-			Vector points = (Vector)_points.elementAt(dataset);
-			for (int index = 0; index < points.size(); index++) {
-				PlotPoint pt = (PlotPoint)points.elementAt(index);
-				if ( minx > pt.x )
-					minx = pt.x;
-				if ( maxx < pt.x )
-					maxx = pt.x;
-			}
-		}
-		setXRange(minx, maxx);
-		setYRange(miny, maxy);
-		repaint();
+	public void zoomYRange(double miny, double maxy) {
+		ptplot.zoomYRange(miny, maxy);
 	}
 	
-	/** The print method copied from Ptolemy. (class PlotBox.ButtonListener) */
-    public void printPtolemyVersion() throws Exception {
-		PrinterJob job = PrinterJob.getPrinterJob();
-		job.setPrintable(this);
-		if (job.printDialog()) 
-			job.print();
-	}
-		
-	/**
-	 * Alternative print option.
-	 * This is trying to overcome a problem with Java printing that
-	 * doesn't take the orientation specified by the user by means
-	 * of job.printDialog().  
-	 * See PlotBox.ButtonListener, DbGui.createPlotPanel.
-	 */
-    public void print() throws Exception {
-		PrinterJob job = PrinterJob.getPrinterJob();
-		PageFormat defaultpage = job.defaultPage();
-		PageFormat format = job.pageDialog(defaultpage);
-		if ( format != defaultpage ) {
-			job.setPrintable(this, format);
-			job.print();
-		}
-	}
-	
-    public synchronized int print(Graphics graphics, PageFormat format, int index) 
-	throws PrinterException  {
-        if (graphics == null)
-			return Printable.NO_SUCH_PAGE;
-			
-        if (index >= 1) {      // We only print on one page.
-            return Printable.NO_SUCH_PAGE;
-        }
-		
-		Graphics2D g2d = (Graphics2D) graphics;
-		if ( antiAliased )
-			g2d.addRenderingHints(hints);
-		
-        g2d.translate((int)format.getImageableX(), (int)format.getImageableY());
-		Rectangle rect = new Rectangle(0,0, 800, 600);
-		double xscale = format.getImageableWidth() / rect.width;
-		//double yscale = format.getImageableHeight() / rect.height;
-		double yscale = xscale;
-		g2d.scale(xscale, yscale);
-		_drawPlot(graphics, true, rect);
-        return Printable.PAGE_EXISTS;
-    }
-
 	public JButton[] getButtons() {
 		return new JButton[] {
 			getButton("print", "Prints the plot"),
@@ -304,34 +400,10 @@ public class Plot extends ptolemy.plot.Plot {
 	}
 
 	public void showPlotFormatter() {
-		PlotFormatter fmt = new PlotFormatter(this);
+		PlotFormatter fmt = new PlotFormatter(ptplot);
 		fmt.openModal();
 	}
 	
-	public void showLegendsWindow() {
-		if ( legendsFrame == null ) {
-			legendsFrame = new JDialog(guidb.parentFrame, false);
-			legendsFrame.setTitle("Legends");
-			legendPanel = new LegendPanel();
-			JScrollPane scroller = new JScrollPane(legendPanel);
-			scroller.setPreferredSize(new Dimension(220,200));
-			legendsFrame.getContentPane().add(scroller);
-			legendsFrame.addWindowListener(new WindowAdapter() {
-				public void windowClosing(WindowEvent e) {
-					legendsFrame.setVisible(false);
-				}
-			});
-			legendsFrame.pack();
-			legendsFrame.setLocation(200, 100);
-		}
-		else if ( legendsFrame.isShowing() ) {
-			legendsFrame.setVisible(false);
-			return;
-		}
-		legendsFrame.setVisible(true);
-		legendPanel.updateLegends();
-	}
-
 	public void exportToEPS() {
 		String filename = Dialogs.selectExportFile("Please give a file name to export the plot");
 		if ( filename == null )
@@ -342,7 +414,7 @@ public class Plot extends ptolemy.plot.Plot {
 				new java.io.BufferedOutputStream(
 					new java.io.FileOutputStream(filename)
 			);
-			this.export(out);
+			ptplot.export(out);
 			out.close();
 		}
 		catch ( Exception ex ) {
@@ -364,54 +436,23 @@ public class Plot extends ptolemy.plot.Plot {
 		}
 	}	
 
-    class MyButtonListener implements ActionListener  {
+	public void fillPlot() {
+		ptplot.fillPlot();
+	}
+		
+    private class MyButtonListener implements ActionListener  {
         public void actionPerformed(ActionEvent event) {
 			String cmd = event.getActionCommand();
             if ( cmd.equals("fill") ) 
-                fillPlot();
+                ptplot.fillPlot();
 			else if ( cmd.equals("print") ) {
 				//guidb.printPlot();
             } 
 			else if ( cmd.equals("reset") )
-                resetAxes();
+                ptplot.resetAxes();
 			else if ( cmd.equals("format") )
 				showPlotFormatter();
         }
     }
 	
-	/** A panel to show the current plot legends. */
-	class LegendPanel extends JPanel {
-		/** Updates the panel contents an calls this.revalidate() and this.repaint(). */
-		void updateLegends() {
-			// subjective values:
-			//	200: enough width for legend texts
-			//	 20: enough height for each legend text
-			
-			int nds = getNumDataSets();
-			Dimension size = new Dimension(200, 10 + 20*nds + 10);
-			setPreferredSize(size);
-			this.revalidate();
-			this.repaint();
-		}
-		
-		public void paint(Graphics graphics) {
-			if ( antiAliased && graphics instanceof Graphics2D )
-				((Graphics2D) graphics).addRenderingHints(hints);
-			super.paint(graphics);
-			Color curr_color = graphics.getColor();
-			int nds = getNumDataSets();
-			for ( int dataset = 0; dataset < nds; dataset++ ) {
-                if (_usecolor) {
-					int color = dataset % _colors.length;
-					graphics.setColor(_colors[color]);
-				}
-				int xpos = 10;
-				int ypos = 10 + dataset * 20;
-				boolean clip = false;
-				_drawPoint(graphics, dataset, xpos, ypos, clip);
-				graphics.setColor(curr_color);
-				graphics.drawString(getLegend(dataset), xpos + 10, ypos + 3);
-			}
-		}
-	}
 }
