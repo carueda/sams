643ca1eda6a77cb7b4450fc450803fed588c4ca6
Carlos Rueda <carueda@gmail.com>
2003-08-20 23:42:55 +0000

    minor change

--- samsgui/src/samsgui/dbgui/Table.java
+++ samsgui/src/samsgui/dbgui/Table.java
@@ -241,7 +241,6 @@ public abstract class Table extends JPanel {
 						sort(sort_tf.getText());
 					}
 					catch(Exception ex) {
-						ex.printStackTrace();
 						SamsGui.message(ex.getMessage());
 					}
 				}
